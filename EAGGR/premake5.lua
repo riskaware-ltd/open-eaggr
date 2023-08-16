dcbd36f2d2385abb6792d21fc3422d13427a7f58
Olek Raymond <123381967+OlekRaymond@users.noreply.github.com>
2023-08-16 11:10:09 +0100

    Update premake5.lua

--- EAGGR/premake5.lua
+++ EAGGR/premake5.lua
@@ -8,7 +8,7 @@ project "OpenEAGGR" -- testing project name
     -- targetdir "../bin/%{cfg.buildcfg}" -- where to compile to
     includedirs { "./", "3rdParty/include/"}
     files { "**.cpp", "**.hpp", "**.h", "**.inl", "**.c" }
-    removefiles { "**/3rdParty/**" } -- Don't compile with these (but they are included)
+    removefiles { "**/3rdParty/**", "./3rdParty/**" } -- Don't compile with these (but they are included)
     warnings "Off"
     vpaths {
         {["*/Header Files"] = {"OpenEAGGR/**.hpp", "OpenEAGGR/**.h"}},
